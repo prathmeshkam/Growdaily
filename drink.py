b451c28872d1c93f5588d362f846bdf3211c9d73
Prathmesh Kamble <95675540+prathmeshkam@users.noreply.github.com>
2022-11-20 16:37:49 +0530

    Add files via upload
    Drink water notification system.

---
+++ drink.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "🥛Please Drink Water Now!!",
+            message ="Drinking Water Helps to Maintain the Balance of Body Fluids.",
+            timeout= 10,
+            app_icon = "D:\Practise Program PY\Project\icon.ico",
+            ticker = "Drink Water Please",
+            )
+        time.sleep(6)
